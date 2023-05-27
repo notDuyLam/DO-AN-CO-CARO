39658a0e4c465a81bafdac5346c098f2058fc378
ilyghost <minhanh120204@gmail.com>
2023-05-27 21:29:52 +0700

    Chỉnh sửa các hàm cho báo cáo

--- DO AN CO CARO/View.h
+++ DO AN CO CARO/View.h
@@ -6,26 +6,20 @@
 void resizeConsole(int width, int height);
 void CreateConsoleWindow(int pWidth, int pHeight); //Tao console voi kich thuoc cho truoc
 void FixConsoleWindow(); //Co dinh console khong cho ng dung thay doi
+void SetColor(WORD color);
+void TextColor(int color);
+void ShowCur(bool CursorVisibility);
 void GotoXY(int x, int y);
 void DrawBoard(int pSize);
-//Hàm xử lý khi người chơi thua
-int ProcessFinish(int pWhoWin, _POINT _A[][BOARD_SIZE], bool& _TURN, int& _X, int& _Y,short int toadothang[24], bool& SoundEffects, _PLAYER& _PLAYER1, _PLAYER& _PLAYER2);
-int AskContinue(_POINT _A[][BOARD_SIZE]);
 void XWin(_POINT _A[][BOARD_SIZE]);
 void OWin(_POINT _A[][BOARD_SIZE]);
 void Draw(_POINT _A[][BOARD_SIZE]);
+void NhapNhayQuanCo(_POINT _A[BOARD_SIZE][BOARD_SIZE], const short int toadothang[24], int pWhoWin);
 void ThongBaoWin(int pWhoWin, _POINT _A[][BOARD_SIZE]);
-void SetColor(WORD color);
 void DrawBoard_1(int m, int n);
-void TextColor(int color);
-void ScreenStartGame(int n, _POINT _A[][BOARD_SIZE], bool _TURN, int _COMMAND, int _X, int _Y, bool validEnter, bool& SoundEffects, int& chedo, _PLAYER& _PLAYER1, _PLAYER& _PLAYER2, int& song, int& songtemp);
-void ShowCur(bool CursorVisibility);
 void ShowTurn(_POINT _A[][BOARD_SIZE], _PLAYER _PLAYER1, _PLAYER _PLAYER2, bool _TURN);
 void PrintText(string text, int color, int x, int y);
 void DrawLoaded(_POINT _A[][BOARD_SIZE]);
-_MENU YesNoMenu(int x, int y);
-_MENU LoadingMenu();
-int EscMenu(_POINT _A[][BOARD_SIZE], bool& SoundEffects, int& song, int& songtemp);
 void HELP();
 void About();
 void Sound(bool& SoundEffects, int& song, int& songtemp);
